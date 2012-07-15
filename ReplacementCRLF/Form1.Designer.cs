6d353d01c9efd63cfc07fa4de16c2a9c36a784aa
unknown <neko@neko-PC.(none)>
2012-07-15 23:43:51 +0900

    ver1.1

--- ReplacementCRLF/Form1.Designer.cs
+++ ReplacementCRLF/Form1.Designer.cs
@@ -32,16 +32,17 @@
             this.InputFileSelect = new System.Windows.Forms.Button();
             this.OutputDir = new System.Windows.Forms.TextBox();
             this.OutputDirSelect = new System.Windows.Forms.Button();
-            this.LF2CRLF = new System.Windows.Forms.CheckBox();
-            this.CRLF2LF = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.ConvertStartBtn = new System.Windows.Forms.Button();
+            this.LF2CRLF = new System.Windows.Forms.RadioButton();
+            this.CRLF2LF = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // InputFile
             // 
             this.InputFile.AllowDrop = true;
-            this.InputFile.Location = new System.Drawing.Point(12, 40);
+            this.InputFile.Location = new System.Drawing.Point(12, 24);
             this.InputFile.Name = "InputFile";
             this.InputFile.Size = new System.Drawing.Size(452, 19);
             this.InputFile.TabIndex = 0;
@@ -50,56 +51,36 @@
             // 
             // InputFileSelect
             // 
-            this.InputFileSelect.Location = new System.Drawing.Point(488, 40);
+            this.InputFileSelect.Location = new System.Drawing.Point(488, 24);
             this.InputFileSelect.Name = "InputFileSelect";
             this.InputFileSelect.Size = new System.Drawing.Size(80, 24);
             this.InputFileSelect.TabIndex = 1;
             this.InputFileSelect.Text = "Select";
             this.InputFileSelect.UseVisualStyleBackColor = true;
+            this.InputFileSelect.Click += new System.EventHandler(this.InputFileSelect_Click);
             // 
             // OutputDir
             // 
             this.OutputDir.AllowDrop = true;
-            this.OutputDir.Location = new System.Drawing.Point(12, 88);
+            this.OutputDir.Location = new System.Drawing.Point(12, 72);
             this.OutputDir.Name = "OutputDir";
             this.OutputDir.Size = new System.Drawing.Size(452, 19);
             this.OutputDir.TabIndex = 2;
-            this.OutputDir.DragDrop += new System.Windows.Forms.DragEventHandler(this.OutputDir_DragDrop);
-            this.OutputDir.DragEnter += new System.Windows.Forms.DragEventHandler(this.OutputDir_DragEnter);
             // 
             // OutputDirSelect
             // 
-            this.OutputDirSelect.Location = new System.Drawing.Point(488, 88);
+            this.OutputDirSelect.Location = new System.Drawing.Point(488, 72);
             this.OutputDirSelect.Name = "OutputDirSelect";
             this.OutputDirSelect.Size = new System.Drawing.Size(80, 24);
             this.OutputDirSelect.TabIndex = 3;
             this.OutputDirSelect.Text = "Select";
             this.OutputDirSelect.UseVisualStyleBackColor = true;
-            // 
-            // LF2CRLF
-            // 
-            this.LF2CRLF.AutoSize = true;
-            this.LF2CRLF.Location = new System.Drawing.Point(16, 128);
-            this.LF2CRLF.Name = "LF2CRLF";
-            this.LF2CRLF.Size = new System.Drawing.Size(86, 16);
-            this.LF2CRLF.TabIndex = 4;
-            this.LF2CRLF.Text = "LF → CRLF";
-            this.LF2CRLF.UseVisualStyleBackColor = true;
-            // 
-            // CRLF2LF
-            // 
-            this.CRLF2LF.AutoSize = true;
-            this.CRLF2LF.Location = new System.Drawing.Point(120, 128);
-            this.CRLF2LF.Name = "CRLF2LF";
-            this.CRLF2LF.Size = new System.Drawing.Size(86, 16);
-            this.CRLF2LF.TabIndex = 5;
-            this.CRLF2LF.Text = "CRLF → LF";
-            this.CRLF2LF.UseVisualStyleBackColor = true;
+            this.OutputDirSelect.Click += new System.EventHandler(this.OutputDirSelect_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 24);
+            this.label1.Location = new System.Drawing.Point(16, 8);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 12);
             this.label1.TabIndex = 6;
@@ -108,28 +89,61 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 72);
+            this.label2.Location = new System.Drawing.Point(16, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(83, 12);
             this.label2.TabIndex = 7;
             this.label2.Text = "OutputDirPath :";
+            // 
+            // ConvertStartBtn
+            // 
+            this.ConvertStartBtn.Location = new System.Drawing.Point(488, 112);
+            this.ConvertStartBtn.Name = "ConvertStartBtn";
+            this.ConvertStartBtn.Size = new System.Drawing.Size(80, 24);
+            this.ConvertStartBtn.TabIndex = 8;
+            this.ConvertStartBtn.Text = "Start";
+            this.ConvertStartBtn.UseVisualStyleBackColor = true;
+            this.ConvertStartBtn.Click += new System.EventHandler(this.ConvertStartBtn_Click);
+            // 
+            // LF2CRLF
+            // 
+            this.LF2CRLF.AutoSize = true;
+            this.LF2CRLF.Location = new System.Drawing.Point(24, 112);
+            this.LF2CRLF.Name = "LF2CRLF";
+            this.LF2CRLF.Size = new System.Drawing.Size(85, 16);
+            this.LF2CRLF.TabIndex = 9;
+            this.LF2CRLF.TabStop = true;
+            this.LF2CRLF.Text = "LF → CRLF";
+            this.LF2CRLF.UseVisualStyleBackColor = true;
+            // 
+            // CRLF2LF
+            // 
+            this.CRLF2LF.AutoSize = true;
+            this.CRLF2LF.Location = new System.Drawing.Point(120, 112);
+            this.CRLF2LF.Name = "CRLF2LF";
+            this.CRLF2LF.Size = new System.Drawing.Size(85, 16);
+            this.CRLF2LF.TabIndex = 10;
+            this.CRLF2LF.TabStop = true;
+            this.CRLF2LF.Text = "CRLF → LF";
+            this.CRLF2LF.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(626, 166);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(597, 148);
             this.Controls.Add(this.CRLF2LF);
             this.Controls.Add(this.LF2CRLF);
+            this.Controls.Add(this.ConvertStartBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.OutputDirSelect);
             this.Controls.Add(this.OutputDir);
             this.Controls.Add(this.InputFileSelect);
             this.Controls.Add(this.InputFile);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "改行置き換え";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,10 +155,11 @@
         private System.Windows.Forms.Button InputFileSelect;
         private System.Windows.Forms.TextBox OutputDir;
         private System.Windows.Forms.Button OutputDirSelect;
-        private System.Windows.Forms.CheckBox LF2CRLF;
-        private System.Windows.Forms.CheckBox CRLF2LF;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ConvertStartBtn;
+        private System.Windows.Forms.RadioButton LF2CRLF;
+        private System.Windows.Forms.RadioButton CRLF2LF;
     }
 }
 
--- ReplacementCRLF/Form1.cs
+++ ReplacementCRLF/Form1.cs
@@ -6,16 +6,63 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+
 
 namespace ReplacementCRLF
 {
     public partial class Form1 : Form
     {
+        #region コンストラクタ
         public Form1()
         {
             InitializeComponent();
         }
+        #endregion
 
+
+        #region ｸﾗｽ・定義
+        public class FilePath
+        {
+            public string InFile        { get; set; }
+            public string InFileName    { get; set; }
+            public string OutDir        { get; set; }
+            public string SelectedPath  { get; set; }
+        } 
+
+        public class ConvertedData
+        {
+            public List<string> AddData { get; set; }
+            public bool IsError { get; set; }
+        }
+
+        public class Log
+        {
+            public int ConvertedCount { get; set; }
+        }
+
+        abstract class myConstants
+        {
+            public const string HeaderIn = " [IN       ] ";
+            public const string HeaderOut = " [OUT      ] ";
+            public const string HeaderDirection = " [DIRECTION] ";
+            public const string HeaderCount = " [COUNT    ] ";
+            public const string LogFilePath = @"\log\ReplacementCRLF.log";
+        }
+
+        private char[] trims = { '\r', '\n' };
+        #endregion
+
+        FilePath CurrentFile = new FilePath();
+        ConvertedData NewLine = new ConvertedData();
+        Timer timer = new Timer();
+        Log ConvertedLog = new Log();        
+
+
+        #region フォーム
+        #region Drag & Drop
         /// <summary>
         /// InputFilePath内にﾄﾞﾗｯｸﾞしたときの動作です。
         /// </summary>
@@ -23,7 +70,30 @@ namespace ReplacementCRLF
         /// <param name="e"></param>
         private void InputFile_DragEnter(object sender, DragEventArgs e)
         {
+            string[] fileNameArray = (string[])e.Data.GetData(DataFormats.FileDrop, false);
 
+            if (!e.Data.GetDataPresent(System.Windows.Forms.DataFormats.FileDrop))
+            {
+                // ファイル以外のドラッグは受け入れない
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+
+            if (fileNameArray.Length > 1)
+            {
+                // 複数ファイルのドラッグは受け入れない
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+            if (Path.GetExtension(fileNameArray[0]) != ".txt")
+            {
+                // 拡張子が.txt以外は受け入れない
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+
+            // 上記以外は受け入れる
+            e.Effect = DragDropEffects.All;
         }
 
         /// <summary>
@@ -33,31 +103,217 @@ namespace ReplacementCRLF
         /// <param name="e"></param>
         private void InputFile_DragDrop(object sender, DragEventArgs e)
         {
+            string[] fileNameArray = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            string fileName = fileNameArray[0];
+
+            this.InputFile.Text = fileName;
+
+            // ﾌｧｲﾙ名取得
+            CurrentFile.InFileName = Path.GetFileNameWithoutExtension(fileName);
+            this.OutputDir.Text = Path.GetDirectoryName(fileName);
+        }
+        #endregion
+
+        #region Select
+        /// <summary>
+        /// "参照"ﾎﾞﾀﾝｸﾘｯｸ時の動作です
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param> 
+        private void InputFileSelect_Click(object sender, EventArgs e)
+        {
+            // OpenFileDialogｸﾗｽのｲﾝｽﾀﾝｽ作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            // はじめのﾌｧｲﾙ名指定
+            //            ofd.FileName = Constants.LogFileName;
+            // はじめに表示されるDir指定(ｶﾚﾝﾄDir)            
+            ofd.InitialDirectory = @"C\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter =
+                "TXTファイル(*.text;*.txt)|*.text;*.txt";
+            // [ﾌｧｲﾙの種類]ではじめに
+            //「すべてのﾌｧｲﾙ」が選択されているようにする
+            ofd.FilterIndex = 2;
+            // ﾀｲﾄﾙ設定
+            ofd.Title = "開くファイルを選択してください";
+            // ﾀﾞｲｱﾛｸﾞﾎﾞｯｸｽを閉じる前に現在のDirを復元するようにする
+            ofd.RestoreDirectory = true;
+            // 存在しないﾌｧｲﾙの名前が指定されたとき警告を表示する
+            ofd.CheckFileExists = true;
+            // 存在しないﾊﾟｽが指定されたとき警告を表示する
+            ofd.CheckPathExists = true;
+
+            // ﾀﾞｲｱﾛｸﾞ表示
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                CurrentFile.InFile = ofd.FileName;
+                this.InputFile.Text = CurrentFile.InFile;
+                // ﾌｧｲﾙ名取得
+                CurrentFile.InFileName = Path.GetFileNameWithoutExtension(CurrentFile.InFile);
+                this.OutputDir.Text = Path.GetDirectoryName(CurrentFile.InFile);
+            }
 
         }
 
         /// <summary>
-        /// OutputDirPath内にﾄﾞﾗｯｸﾞしたときの動作です。
+        /// 出力先ﾃﾞｨﾚｸﾄﾘを指定します
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>        
-        private void OutputDir_DragEnter(object sender, DragEventArgs e)
+        private void OutputDirSelect_Click(object sender, EventArgs e)
         {
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            //デフォルトでDesktop
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            //最初に選択するフォルダを指定する
+            //RootFolder以下にあるフォルダである必要がある
+            fbd.SelectedPath = System.IO.Directory.GetCurrentDirectory();
+            //ユーザーが新しいフォルダを作成できるようにする
+            //デフォルトでTrue
+            fbd.ShowNewFolderButton = true;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                CurrentFile.SelectedPath = fbd.SelectedPath;
+                //選択されたフォルダを表示する
+                this.OutputDir.Text = CurrentFile.SelectedPath;
+            }
+
 
         }
+        #endregion
 
         /// <summary>
-        /// OutputDirPath内にﾄﾞﾛｯﾌﾟしたときの動作です。
+        /// 変換開始ﾎﾞﾀﾝｸﾘｯｸ時の動作です。
         /// </summary>
         /// <param name="sender"></param>
-        /// <param name="e"></param>        
-        private void OutputDir_DragDrop(object sender, DragEventArgs e)
+        /// <param name="e"></param>
+        private void ConvertStartBtn_Click(object sender, EventArgs e)
         {
 
+            // OpenFileDialogｸﾗｽのｲﾝｽﾀﾝｽ作成
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.FileName = this.InputFile.Text;
+
+            System.IO.Stream stream = ofd.OpenFile();
+            //内容を読み込む
+            System.IO.StreamReader sr =
+                new System.IO.StreamReader(
+                    stream,
+                    System.Text.Encoding.GetEncoding("shift-jis"));
+
+
+            if (this.OutputDir.Text == "")
+            {
+                this.OutputDir.Text = Directory.GetCurrentDirectory();
+            }
+
+            // ﾌｧｲﾙ作成
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(
+                this.OutputDir.Text + @"\" + CurrentFile.InFileName + "new.txt",
+                true,
+                System.Text.Encoding.GetEncoding("shift-jis"));
+
+            string line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                NewLine.AddData = new List<string>();
+
+                string buf = line.Replace("\r", "").Replace("\n", "");
+                NewLine.AddData.Add(buf);
+                
+                if (this.CRLF2LF.Checked)
+                {
+                    NewLine.AddData.Add("\r");
+                    NewLine.IsError = true;
+                }
+                else if (this.LF2CRLF.Checked)
+                {
+                    NewLine.AddData.Add("\r\n");
+                    NewLine.IsError = true;
+                }
+                else
+                {
+                    MessageBox.Show("変換方法を選択してください",
+                        "エラー",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                    NewLine.IsError = false;
+                    break;
+                }
+
+                NewLine.AddData.ForEach((string str) => sw.Write(str));
+                ConvertedLog.ConvertedCount++;
+            }
+            sr.Close();
+            sw.Close();
+            stream.Close();
+
+            if (NewLine.IsError == true)
+            {
+                MessageBox.Show("変換が完了しました！",
+                                "完了",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+
+            UpdateLog();
         }
+        #endregion
 
-  
 
+        /// <summary>
+        /// ログ更新
+        /// </summary>
+        private void UpdateLog()
+        {
+            // ﾌｧｲﾙ作成
+            System.IO.StreamWriter logFile = new System.IO.StreamWriter(
+                Directory.GetCurrentDirectory() + myConstants.LogFilePath,
+                true,
+                System.Text.Encoding.GetEncoding("shift-jis"));
+ 
+            logFile.Write(System.DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"));
+            logFile.Write(myConstants.HeaderIn);
+            logFile.WriteLine(this.InputFile.Text);
+
+
+            logFile.Write(System.DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"));
+            logFile.Write(myConstants.HeaderOut);
+            logFile.WriteLine(this.OutputDir.Text + @"\" + 
+                CurrentFile.InFileName + "new.txt");
+
+
+            logFile.Write(System.DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"));
+            logFile.Write(myConstants.HeaderDirection);
+            if (this.CRLF2LF.Checked)
+            {
+                logFile.WriteLine("CRLF→LF");
+            }
+            else if (this.LF2CRLF.Checked)
+            {
+                logFile.WriteLine("LF→CRLF");
+            }
+            else
+            {
+                logFile.WriteLine("ERROR");
+            }
+
+
+            logFile.Write(System.DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"));
+            logFile.Write(myConstants.HeaderCount);
+            logFile.WriteLine(ConvertedLog.ConvertedCount);
+            logFile.Close();
+
+        }
 
 
     }
